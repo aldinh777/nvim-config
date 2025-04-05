91d219291838715c6eca9e99a40cdf8a1e906d51
aldinh777 <aldi.nh789@gmail.com>
2025-04-06 01:33:33 +0800

    i use vim btw

---
+++ lua/plugins/themes.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+  },
+  {
+    "Mofiqul/vscode.nvim",
+    name = "vscode-theme",
+  },
+  {
+    "patstockwell/vim-monokai-tasty",
+    name = "monokai-tasty",
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+  },
+}
