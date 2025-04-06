8f35650d930de2d6e07ba2c5769026cd17fee5a1
aldinh777 <aldi.nh789@gmail.com>
2025-04-06 12:59:09 +0800

    markdown formatter

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -4,6 +4,7 @@ return {
     opts.formatters_by_ft = vim.tbl_extend("force", opts.formatters_by_ft or {}, {
       json = { "prettierd" },
       jsonc = { "prettierd" },
+      markdown = { "prettierd" },
     })
   end,
 }
