8e46cb7a0ef83dac6369c2164430937d457c26cf
aldinh777 <aldi.nh789@gmail.com>
2025-10-02 03:50:51 +0800

    add clangd and remove copilot again

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  callback = function()
+    vim.cmd([[
+      hi Normal guibg=none ctermbg=none
+      hi NormalFloat guibg=none ctermbg=none
+    ]])
+  end,
+})
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,7 +21,7 @@ require("lazy").setup({
       "LazyVim/LazyVim",
       import = "lazyvim.plugins",
       opts = {
-        colorscheme = "rose-pine-moon",
+        colorscheme = "vscode",
       },
     },
     -- import/override with your plugins
